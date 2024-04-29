e61e5cc496cd6def6dd58050b2466dd32893c4e4
selvuen <azozy2011.hd@gmail.com>
2024-04-29 23:26:04 +0300

    more fix

--- utils/tweet.py
+++ utils/tweet.py
@@ -3,19 +3,19 @@ import re
 regex = r"https?://(?:twitter|X|vxtwitter|fxtwitter)\.com/[A-Za-z0-9_]+/status/(\d+)"
 
 
-def find_tweet(message: str) -> re.Match[str] | None:
+def find_tweet(message: str):
     match = re.search(pattern=regex, string=message, flags=re.IGNORECASE)
     return match if match else None
 
 
-def get_tweet_id(message: str) -> str | None:
+def get_tweet_id(message: str):
     match = find_tweet(message)
     if match:
         return match.group(1)
     return None
 
 
-def get_tweet_link(message: str) -> str | None:
+def get_tweet_link(message: str):
     match = find_tweet(message)
     if match:
         return match.group(0)
